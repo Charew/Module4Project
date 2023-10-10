4a02d9ffab005b272afa940e3275e40f310c9b24
Charew <144203406+Charew@users.noreply.github.com>
2023-10-10 11:27:30 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,6 @@
-# Need requests to use the API, need BaseHTTPRequestHandler to use vercel serverless function
 import requests
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-# Class handler invokes a serverless function in vercel. I had to read up on their documentation to figure this out
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
@@ -16,8 +14,7 @@ class handler(BaseHTTPRequestHandler):
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write("Not Found".encode())
-   
-    # Our original IP function from part 1
+
     def get_public_ip_info(self):
         url = "http://ip-api.com/json/"
 
@@ -32,7 +29,7 @@ class handler(BaseHTTPRequestHandler):
                 result += f"City: {ip_info['city']}\n"
                 result += f"Region: {ip_info['regionName']}\n"
                 result += f"Country: {ip_info['country']}\n"
-                result += f"ISP: {ip_info['isp']}"
+                result += f"ISP: {ip_info['isp']}\n"
                 return result
             else:
                 return "Failed to retrieve IP information."
@@ -40,10 +37,9 @@ class handler(BaseHTTPRequestHandler):
         except requests.exceptions.RequestException as e:
             return f"Error: {e}"
 
-# The serverless function definition and function itself
 def run_vercel_server():
     server_address = ("0.0.0.0", 3000)
-    httpd = HTTPServer(server_address, IPCollectorHandler)
+    httpd = HTTPServer(server_address, handler)
     httpd.serve_forever()
 
 if __name__ == "__main__":
